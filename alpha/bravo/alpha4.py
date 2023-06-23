636de7d99ffacd22f08a950d4266d736605e6efb
Renegmz <renegmz21@gmail.com>
2023-06-23 01:13:08 -0400

    [ADD]: hacks

---
+++ alpha/bravo/alpha4.py
@@ -0,0 +1,3 @@
+lista = [100, 200, 300, 400, 500, 600, 700]
+lista = [x for x in lista if x not in [300, 500]]
+print(lista)
---
+++ alpha/bravo/alpha5.py
@@ -0,0 +1,3 @@
+listaA = [100, 200, 300, 400, 500, 600, 700]
+listaB = listaA[:]
+print(listaB)
