03a9908aadd5d81bcd9fe5d47e1ec43887d22dc9
celestoM <celesto1@gmail.com>
2023-06-22 21:10:32 -0400

    feat(new) charlie-hacks

---
+++ charlie/hack1.py
@@ -0,0 +1,6 @@
+# HACK-1 
+##vaciar la lista
+#[100,200,300,400,500,600,700]  result >  []  
+lista=[100,200,300,400,500,600,700]
+lista.clear()
+print(lista)
---
+++ charlie/hack4.py
@@ -0,0 +1,7 @@
+#HACK-4
+#eliminar los items 300 y 500
+#[100,200,300,400,500,600,700]  result >  [100,200,400,600,700]
+lista = [100, 200, 300, 400, 500, 600, 700]
+lista.remove(300)
+lista.remove(500)
+print(lista)
